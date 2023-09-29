0c4c16a5560a5c2d3c0e2f326dd8e5c536f0edfb
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-29 21:09:46 +0330

    Add files via upload

---
+++ WindowsFormsApp115/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp115
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cEmployee x = new cEmployee();
+            cEmployee y = new cEmployee();
+
+            MessageBox.Show(x.GetHashCode().ToString());
+        }
+    }
+}
